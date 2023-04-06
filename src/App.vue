0f9e239a9ed1fd7f8a0e4b5c8ce735f1e1f60394
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-06 10:07:08 -0300

    refactor(App.vue): replace HelloWorld component with AgeCard component style(App.vue): add container class with styles for the main container

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import AgeCard from './components/AgeCard.vue'
+
+export default {
+  components: {
+    Card: AgeCard,
+  },
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container">
+    <Card/>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style lang="scss" scoped>
+.container {
+  background-color: var(--off-white);
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
 }
 </style>
