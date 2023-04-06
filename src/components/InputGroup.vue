26f7287747103937ac69502a1eb7d447d6786e03
Marcus Xavier <marcusxavierr123@gmail.com>
2023-04-06 10:07:55 -0300

    feat: Add new InputGroup component with props for modelValue and hasError and a template for rendering input fields and labels in the src/components directory

---
+++ src/components/InputGroup.vue
@@ -0,0 +1,61 @@
+<script lang="ts">
+export default {
+  props: ['modelValue', 'hasError'],
+  emits: ['update:modelValue'],
+  mounted() {
+    console.log(this.modelValue)
+  }
+}
+</script>
+
+<template>
+  <div class="input-container">
+    <div class="input-group">
+      <label for="day">DAY</label>
+      <input type="text" v-model="modelValue.day" />
+    </div>
+    <div class="input-group">
+      <label for="month">MONTH</label>
+      <input type="text" v-model="modelValue.month" />
+    </div>
+    <div class="input-group">
+      <label for="year">YEAR</label>
+      <input type="text" v-model="modelValue.year"/>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.input-container {
+  /* max-width: 640px; */
+  display: flex;
+  gap: 1rem;
+}
+
+.input-group {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+}
+
+label {
+  font-weight: 800;
+  color: var(--smokey-grey);
+}
+
+input {
+  font-size: 1.5rem;
+  border: 1px solid var(--light-grey);
+  padding: 0.75rem 1rem;
+  border-radius: 0.5rem;
+  flex-grow: 1;
+  width: 5.5rem;
+}
+
+@media (min-width: 840px) {
+  input {
+    width: 10rem;
+    font-size: 2rem;
+  }
+}
+</style>
